daef80cf777131d0b93611fed38226863346f550
Scott Francis <sgf14@netzero.com>
2020-11-13 21:44:53 -0700

    hello test

--- PictureBook/src/test/java/com/critter/cam/MainTest.java
+++ PictureBook/src/test/java/com/critter/cam/MainTest.java
@@ -1,5 +1,6 @@
 package com.critter.cam;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,11 @@ public class MainTest {
 	
 	@Test
 	void isJunitWorking() {    	
+        assertTrue(true);
+    }
+	
+	@Test
+	void helloTest() {    	
         assertEquals("hello to you", Main.introduction("hi"));
     }
 
